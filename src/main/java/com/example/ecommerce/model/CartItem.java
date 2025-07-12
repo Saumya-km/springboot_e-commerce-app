17bf270d72af9d8b5f4073498fe528786b6fb3d8
Saumya-km <saumyasingh9653@gmail.com>
2025-07-12 22:15:55 +0530

    Update CartItem.java

--- src/main/java/com/example/ecommerce/model/CartItem.java
+++ src/main/java/com/example/ecommerce/model/CartItem.java
@@ -1,1 +1,14 @@
+@Entity
+public class CartItem {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
+  @ManyToOne
+  private Product product;
+
+  @ManyToOne
+  private User user;
+
+  private int quantity;
+}
