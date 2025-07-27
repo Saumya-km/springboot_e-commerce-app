285476f005efd7defbf8d88c8614ea4aa46f62f6
Saumya-km <saumyasingh9653@gmail.com>
2025-07-27 23:33:33 +0530

    Update User.java

--- src/main/java/com/example/ecommerce/model/User.java
+++ src/main/java/com/example/ecommerce/model/User.java
@@ -1,1 +1,15 @@
+package com.example.ecommerce.model;
 
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    // getters and setters
+}
